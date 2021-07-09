b4fc566f2b33295cad051d02d152908311b70cf0
Eleven <idiotalex@163.com>
2021-07-10 00:55:17 +0800

    add configure files

--- chapter-13/src/main/java/com/hotstrip/code/design/chapter13/good/config/StarterAutoConfigure.java
+++ chapter-13/src/main/java/com/hotstrip/code/design/chapter13/good/config/StarterAutoConfigure.java
@@ -1,4 +1,25 @@
 package com.hotstrip.code.design.chapter13.good.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration
+@ConditionalOnClass(StarterService.class)
+@EnableConfigurationProperties(StarterServiceProperties.class)
 public class StarterAutoConfigure {
+    @Resource
+    private StarterServiceProperties starterServiceProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "itstack.door", value = "enabled", havingValue = "true")
+    StarterService starterService() {
+        return new StarterService(starterServiceProperties.getUserStr());
+    }
 }
--- chapter-13/src/main/java/com/hotstrip/code/design/chapter13/good/config/StarterServiceProperties.java
+++ chapter-13/src/main/java/com/hotstrip/code/design/chapter13/good/config/StarterServiceProperties.java
@@ -1,4 +1,16 @@
 package com.hotstrip.code.design.chapter13.good.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("itstack.door")
 public class StarterServiceProperties {
+    private String userStr;
+
+    public String getUserStr() {
+        return userStr;
+    }
+
+    public void setUserStr(String userStr) {
+        this.userStr = userStr;
+    }
 }

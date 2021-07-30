58ddb16cdea4debccb06acad0bca2e8309cfc634
Eleven <idiotalex@163.com>
2021-07-30 23:59:46 +0800

    add test class

---
+++ chapter-18/src/test/java/com/hotstrip/code/design/chapter18/base/IteratorTest.java
@@ -0,0 +1,20 @@
+package com.hotstrip.code.design.chapter18.base;
+
+import com.hotstrip.code.design.chapter18.group.GroupStructure;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@Slf4j
+public class IteratorTest {
+
+    @Test
+    public void test() {
+        // 数据填充
+        GroupStructure groupStructure = new GroupStructure("1", "小傅哥");
+        // 雇员信息
+
+    }
+
+}
